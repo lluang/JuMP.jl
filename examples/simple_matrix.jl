986615ee3d90135be4b5c3fafdb3538a984b3824
Joey Huchette <joehuchette@gmail.com>
2015-06-06 22:43:54 -0400

    add a few more tests

--- examples/simple_matrix.jl
+++
@@ -1,9 +0,0 @@
-using JuMP
-
-m = Model()
-@defVar(m, X[1:3,1:3], SDP)
-
-@addSDPConstraint(m, X >= ones(3,3))
-@setObjective(m, Min, trace(ones(3,3)*X))
-
-stat = solve(m)
--- test/sdp.jl
+++ test/sdp.jl
@@ -61,9 +61,65 @@ context("With solver $(typeof(solver))") do
     @fact trace(XX) => roughly( 1, 1e-5)
     @fact trace(YY) => roughly( 3, 1e-5)
     @fact trace(ZZ) => roughly(-1, 1e-5)
+
+    # Test SDP constraints
+    m = Model()
+    @defVar(m, X[1:3,1:3], SDP)
+
+    @addSDPConstraint(m, X >= ones(3,3))
+    @setObjective(m, Min, trace(ones(3,3)*X))
+    stat = solve(m)
+    @fact stat => :Optimal
+    XX = getValue(X)
+    @fact ispsd(XX) => true
+    @fact ispsd(XX - ones(3,3)) => true
+    @fact norm(XX-[23 2 2;2 23 2;2 2 23]./9) => roughly(0,1e-3)
+
+    # Another test SDP
+    m = Model()
+    @defVar(m, X[1:3,1:3], SDP)
+    @defVar(m, Y[1:2,1:2], SDP)
+
+    C = eye(3,3)
+    A1 = zeros(3,3)
+    A1[1,1] = 1.0
+    A2 = zeros(3,3)
+    A2[2,2] = 1.0
+    A3 = zeros(3,3)
+    A3[3,3] = 1.0
+    D = eye(2,2)
+    B1 = ones(2,2)
+    B2 = zeros(2,2)
+    B2[1,1] = 1
+    B3 = zeros(2,2)
+    B3[1,2] = B3[2,1] = 2
+
+    @setObjective(m, Min, trace(C*X)+1+trace(D*Y))
+    @addConstraint(m, trace(A1*X-eye(3,3)/3) == 0)
+    @addConstraint(m, 2*trace(A2*X) == 1)
+    @addConstraint(m, trace(A3*X) >= 2)
+    @addConstraint(m, trace(B1*Y) == 1)
+    @addConstraint(m, trace(B2*Y) == 0)
+    @addConstraint(m, trace(B3*Y) <= 0)
+    @addConstraint(m, trace(A1*X)+trace(B1*Y) >= 1)
+    @addConstraint(m, Y[2,2] == 1)
+
+    stat = solve(m)
+    @fact stat => :Optimal
+    XX, YY = getValue(X), getValue(Y)
+    @fact trace(A1*XX-eye(3,3)/3) => roughly(0, 1e-8)
+    @fact 2*trace(A2*XX) => roughly(1, 1e-8)
+    @fact trace(A3*XX) >= 2 => true
+    @fact trace(B1*YY) => roughly(1, 1e-8)
+    @fact trace(B2*YY) => roughly(0, 1e-8)
+    @fact trace(B3*YY) <= 0 => true
+    @fact trace(A1*XX)+trace(B1*YY) >= 1 => true
+    @fact YY[2,2] => roughly(1, 1e-8)
+    @fact norm(XX - diagm([1,.5,2])) => roughly(0, 1e-4)
+    @fact norm(YY - [0 0;0 1]) => roughly(0, 1e-4)
 end; end; end
 
-facts("[sdp] Nonsensical SDP variable construction") do
+facts("[sdp] Nonsensical SDPs") do
     m = Model()
     @fact_throws @defVar(m, unequal[1:5,1:6], SDP)
     # Some of these errors happen at compile time, so we can't use @fact_throws
@@ -75,4 +131,10 @@ facts("[sdp] Nonsensical SDP variable construction") do
     @fact_throws @defVar(m, -ones(4,4) <= foo[1:4,1:4] <= ones(4,5), SDP)
     @fact_throws @defVar(m, -rand(5,5) <= nonsymmetric[1:5,1:5] <= rand(5,5), SDP)
     @fact_throws @defVar(m, -1.0 <= nonzero[1:6,1:6] <= 1.0, SDP)
+
+    # test that nonsymmetric constraints throw
+    @defVar(m, X[1:5,1:5], SDP)
+    Y = eye(5)*X
+    Y[1,2] += 1
+    @fact_throws @addSDPConstraint(m, Y <= ones(5,5))
 end
